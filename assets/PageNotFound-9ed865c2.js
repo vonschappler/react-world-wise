aad8e27dfd2c4802f2fd5f0608e7d5f063d62aea
vonschappler <vonschappler@users.noreply.github.com>
2023-12-12 19:57:27 +0000

    deploy: 084b8f5bcc00e1f42ff205bf07c49b28e59b06ec

--- assets/PageNotFound-9ed865c2.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-0d69a22b.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
---
+++ assets/PageNotFound-d066d85c.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-3409f61c.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
