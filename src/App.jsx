154a45858263a1e57f7a852bf17864543da1e25e
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-12-12 11:47:13 -0300

    Vite depoyment test

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import City from './components/City';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
-// import Form from './components/Form';
+import Form from './components/Form';
 import { CitiesProvider } from './contexts/CitiesContext';
 import { AuthProvider } from './contexts/FakeAuthContext';
 import ProtectedRoute from './pages/ProtectedRoute';
@@ -47,7 +47,7 @@ function App() {
                 <Route path='cities' element={<CityList />} />
                 <Route path='cities/:id' element={<City />} />
                 <Route path='countries' element={<CountryList />} />
-                {/* <Route path='form' element={<Form />} /> */}
+                <Route path='form' element={<Form />} />
               </Route>
               <Route path='*' element={<PageNotFound />} />
             </Routes>
